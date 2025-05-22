10d131d3af11a2b16cae4ca211f5dab5da3f4591
sbharathidasan <bharathidasan2072005@gmail.com>
2025-05-22 18:35:49 +0530

    created the api call and stored it in the movie object

--- netflix/src/main/java/com/movie/netflix/Service/ServiceClass.java
+++ netflix/src/main/java/com/movie/netflix/Service/ServiceClass.java
@@ -4,6 +4,7 @@ import com.movie.netflix.display.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.URI;
@@ -21,32 +22,19 @@ public class ServiceClass {
     private Movie movie;
 
     public Movie returnMovieDetail(String name){
-
-        try {
-            getImdbRequest(name);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        movie.setName(name);
-        return movie;
-    }
-    private void getImdbRequest(String name) throws IOException, InterruptedException {
         String link="https://www.omdbapi.com/?t="+name+"&apikey="+api;
-        URI uri=URI.create(link);
-        HttpClient client=HttpClient.newHttpClient() ;
-        HttpRequest request=HttpRequest.newBuilder()
-                .GET()
-                .uri(uri)
-                .header("Accept","application/json")
-                .timeout(Duration.ofSeconds(10))
-                .build();
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
-        System.out.println(response.body());
         System.out.println(link);
+        RestTemplate restTemplate=new RestTemplate();
+        movie =restTemplate.getForObject(link,Movie.class);
+        System.out.println(movie);
+        return movie;
     }
 
 
 }
+//public Movie fetchMovieFromOMDb(String title) {
+//    String url = "https://www.omdbapi.com/?t=" + title + "&apikey=" + apiKey;
+//    RestTemplate restTemplate = new RestTemplate();
+//    Movie movie = restTemplate.getForObject(url, Movie.class);
+//    return movie;
+//}
